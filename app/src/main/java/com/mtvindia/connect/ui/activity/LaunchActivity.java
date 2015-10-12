7779f87b9ff595dfd4d14d0063805fa3bd6714c0
Aaditya <aaditya.deowanshi@tecsolsoftware.com>
2015-10-12 17:21:16 +0530

    Modified full screen
    changed styles xml file for full screen feature

--- app/src/main/java/com/mtvindia/connect/ui/activity/LaunchActivity.java
+++ app/src/main/java/com/mtvindia/connect/ui/activity/LaunchActivity.java
@@ -2,8 +2,6 @@ package com.mtvindia.connect.ui.activity;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.Window;
-import android.view.WindowManager;
 
 import com.mtvindia.connect.R;
 
@@ -11,8 +9,6 @@ public class LaunchActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        requestWindowFeature(Window.FEATURE_NO_TITLE);
-        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_launch);
     }
