3ea4ccb175945c44c383507da5eb0d7a296abd1f
Aaditya <aaditya.deowanshi@tecsolsoftware.com>
2015-10-13 10:28:41 +0530

    Connected launch screen with login screen
    moving from launch screen to login screen with splash time out of 3sec

--- app/src/main/java/com/mtvindia/connect/ui/activity/LaunchActivity.java
+++ app/src/main/java/com/mtvindia/connect/ui/activity/LaunchActivity.java
@@ -1,16 +1,27 @@
 package com.mtvindia.connect.ui.activity;
 
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+import android.os.Handler;
 
 import com.mtvindia.connect.R;
+import com.mtvindia.connect.app.base.BaseActivity;
 
-public class LaunchActivity extends AppCompatActivity {
+public class LaunchActivity extends BaseActivity {
+
+    private static int timeOut = 3000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_launch);
+
+        new Handler().postDelayed(new Runnable() {
+
+            @Override
+            public void run() {
+                startActivity(LoginActivity.class, null);
+            }
+        }, timeOut);
     }
 
 }
