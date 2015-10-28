9d6ccb98386d78fdf4cdace04196abfc2f27312a
Aaditya <aaditya.deowanshi@tecsolsoftware.com>
2015-10-28 20:27:11 +0530

    Api files for making network calls

--- app/src/main/java/com/mtvindia/connect/app/Config.java
+++ app/src/main/java/com/mtvindia/connect/app/Config.java
@@ -10,17 +10,18 @@ public class Config {
     //--------------------------------------------------------------------------------
     // API related constants/configurations - used in ApiModule
     //--------------------------------------------------------------------------------
-    public static final String BASE_URL_PRODUCTION  = "http://mtvapi.getsandbox.com";
+    //public static final String BASE_URL_PRODUCTION  = "http://mtvLoginApi.getsandbox.com";
+    public static final String BASE_URL_PRODUCTION  = "http://mtvlogin.getsandbox.com";
     public static final String BASE_URL_DEVELOP     = "";
 
     public static final String BASE_URL             = BASE_URL_PRODUCTION;
+
+    public static final String USER_LOGIN           = "/login";
 
     // Cache size in bytes, 50 MB = 50 * 1024 KB, 1 KB = 1024 Bytes
     public static final int HTTP_DISK_CACHE_SIZE    = (int) 50 * 1024 * 1024;
 
     // User agent - must required for some api
     public static final String USER_AGENT           = "MtvConnect-Android-App";
-
-    public static final String USER_LOGIN           = "/login";
 
 }
