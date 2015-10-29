86ecac1ed41735fd8e3e8bbcb2d24e23a1ca7a37
Aaditya <aaditya.deowanshi@tecsolsoftware.com>
2015-10-29 12:12:37 +0530

    Update api calls files created

---
+++ app/src/main/java/com/mtvindia/connect/presenter/UpdatePresenter.java
@@ -0,0 +1,9 @@
+package com.mtvindia.connect.presenter;
+
+import com.mtvindia.connect.app.base.Presenter;
+
+/**
+ * Created by Sibi on 28/10/15.
+ */
+public interface UpdatePresenter extends Presenter<UpdateViewInteractor> {
+}
---
+++ app/src/main/java/com/mtvindia/connect/presenter/concrete/UpdatePresenterImpl.java
@@ -0,0 +1,13 @@
+package com.mtvindia.connect.presenter.concrete;
+
+import com.mtvindia.connect.app.base.BaseNetworkPresenter;
+import com.mtvindia.connect.presenter.UpdatePresenter;
+import com.mtvindia.connect.presenter.UpdateViewInteractor;
+
+/**
+ * Created by Sibi on 28/10/15.
+ */
+public class UpdatePresenterImpl extends BaseNetworkPresenter<UpdateViewInteractor> implements UpdatePresenter {
+
+
+}
